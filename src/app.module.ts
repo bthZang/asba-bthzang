3107817409411a1d2ebf34e4093b65bd1a5a08b4
Zang <bt.hoanggiang@gmail.com>
2024-01-31 20:36:31 +0700

    add lecturer controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SubjectController } from './components/subject/subject.controller';
+import { LecturerController } from './components/lecturer/lecturer.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, SubjectController, LecturerController],
   providers: [AppService],
 })
 export class AppModule {}
