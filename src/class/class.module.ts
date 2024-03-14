d942860c46bb456b80491b85ac781f61e9175170
Zang <bt.hoanggiang@gmail.com>
2024-03-15 05:49:13 +0700

    fix: remove circular dependencies of class module

--- src/class/class.module.ts
+++ src/class/class.module.ts
@@ -1,13 +1,21 @@
-import { Module } from '@nestjs/common';
-import { ClassService } from './class.service';
-import { ClassResolver } from './class.resolver';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Class } from './entities/class.entity';
+import { LecturerModule } from 'src/lecturer/lecturer.module';
 import { PointModule } from 'src/point/point.module';
 import { SemesterModule } from 'src/semester/semester.module';
+import { SubjectModule } from 'src/subject/subject.module';
+import { ClassResolver } from './class.resolver';
+import { ClassService } from './class.service';
+import { Class } from './entities/class.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Class]), PointModule, SemesterModule],
+  imports: [
+    TypeOrmModule.forFeature([Class]),
+    forwardRef(() => LecturerModule),
+    forwardRef(() => SubjectModule),
+    forwardRef(() => SemesterModule),
+    forwardRef(() => PointModule),
+  ],
   providers: [ClassResolver, ClassService],
   exports: [TypeOrmModule.forFeature([Class]), ClassService],
 })
